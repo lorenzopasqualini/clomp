2584f459129772e2f72b3498c9d7bed56bec1e33
Lorenzo Pasqualini <lorenzopasqualini@outlook.com>
2023-08-06 16:25:46 +0200

    v0.8

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -8,6 +8,7 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
 	({ className, children, disabled, type = 'button', ...props }, ref) => {
 		return (
 			<button
+				type={type}
 				ref={ref}
 				disabled={disabled}
 				className={cn(
